c328ffd1344b094a26b5ccda0a50ad35c0fb7c19
Maryam-bit <rajputmaryam5555@gmail.com>
2023-01-14 18:47:18 +0500

    function types

---
+++ Understanding-TS/lecture10.ts
@@ -0,0 +1,22 @@
+function add(n1: number, n2: number) {
+    return n1 + n2;
+}
+
+function printResult(num: number) {
+    console.log("Result: " + num);
+}
+
+let combineValue: Function;
+combineValue = add;
+combineValue = printResult;
+// combineValue = 5 !!! Error !!!
+console.log(combineValue(9, 9)) // undefined since print result accept one arg
+
+// function type
+// Function types are the types that describe the function regarding parameters and return value of the function
+
+let combineValue2: (a: number, b: number) => number;
+combineValue2 = add;
+// combineValue2 = printResult; // !!! ERROR //
+// combineValue2 = 5 // !!! Error !!!
+console.log(combineValue2(9, 9)) // undefined since print result accept one arg
