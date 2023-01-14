b35728a2d949e2220a7401d3b015df1b1ad83267
Maryam-bit <rajputmaryam5555@gmail.com>
2023-01-14 18:54:29 +0500

    function type and callback

---
+++ Understanding-TS/lecture11.ts
@@ -0,0 +1,10 @@
+//  function types and callback
+
+function addAndHandle (n1: number, n2: number, cb: (num: number) => void) {
+    const result = n1 + n2;
+    cb(result);
+}
+
+addAndHandle(10, 20, (result) => {
+    console.log(result);
+})
