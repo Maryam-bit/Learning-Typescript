5ccd7aa3eb5f35a6e58b7c552b3a4670b9bb3796
Maryam-bit <rajputmaryam5555@gmail.com>
2023-01-17 23:47:54 +0500

    this keyword ts classes

--- Understanding-TS/Lecture14.ts
+++ Understanding-TS/Lecture14.ts
@@ -7,8 +7,17 @@ class Department {
     constructor (n: string) {
         this.name = n;
     }
+
+    // method
+    describe (this: Department) {
+        console.log("Department: " + this.name)
+    }
 }
 
 // now create  department object
 const accounting = new Department("Accounting");
-console.log(accounting)
+accounting.describe();
+
+const accountingCopy = { name: "abc", describe: accounting.describe };
+// we are calling describe from accounting copy on which "this" doesn't exists, this can be fix by adding "this" parameter in describe method in constructor
+accountingCopy.describe() // Department: undefined
