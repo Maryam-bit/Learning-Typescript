a229695dc188dd3e05dd1a0b0a801a50ac4a5b31
Maryam-bit <rajputmaryam5555@gmail.com>
2023-01-17 23:56:55 +0500

    ts private and public access modifiers

--- Understanding-TS/Lecture14.ts
+++ Understanding-TS/Lecture14.ts
@@ -4,6 +4,8 @@
 // constructor is a method, which is a reserved word
 class Department {
     name: string;
+    // make the employees private so that they can only be accessible inside the class
+    private employees: string[] = [];
     constructor (n: string) {
         this.name = n;
     }
@@ -12,12 +14,27 @@ class Department {
     describe (this: Department) {
         console.log("Department: " + this.name)
     }
+
+    addEmployee(employee: string) {
+        this.employees.push(employee)
+    }
+
+    printEmployeeInformation() {
+        console.log(this.employees.length)
+        console.log(this.employees)
+    }
 }
 
 // now create  department object
 const accounting = new Department("Accounting");
+accounting.addEmployee("Max");
+accounting.addEmployee("Manu");
+
+// accounting.employees[2] = "anna";  // error because you cannot change employee value outside the class
+
 accounting.describe();
+accounting.printEmployeeInformation();
 
 const accountingCopy = { name: "abc", describe: accounting.describe };
 // we are calling describe from accounting copy on which "this" doesn't exists, this can be fix by adding "this" parameter in describe method in constructor
-accountingCopy.describe() // Department: undefined
+// accountingCopy.describe() // Department: undefined
