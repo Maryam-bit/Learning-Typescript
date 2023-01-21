1ad0196a992acd5274087d97d22dd962ef44268b
Maryam-bit <rajputmaryam5555@gmail.com>
2023-01-22 02:55:37 +0500

    generic classes

---
+++ Understanding-TS/lecture37.ts
@@ -0,0 +1,27 @@
+// generics classes
+
+class DataStorage<T extends string | number | boolean> {
+    private data: T[] = [];
+
+    addItem(item: T) {
+        this.data.push(item)
+    }
+
+    removeItem(item: T) {
+        if(this.data.indexOf(item) === -1) {
+            this.data.splice(this.data.indexOf(item), 1)
+        }
+    }
+    
+    getItems() {
+        return [...this.data]
+    }
+}1
+
+const textStorage = new DataStorage<string>();
+textStorage.addItem("Max")
+textStorage.addItem("Manu")
+textStorage.removeItem("Max")
+console.log(textStorage.getItems());
+
+const numberStorage = new DataStorage<number | string>();
