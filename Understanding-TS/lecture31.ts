91b278f888347b8eb90cd9a74f33ac40278731d7
Maryam-bit <rajputmaryam5555@gmail.com>
2023-01-22 00:51:46 +0500

    Nullish coalescing

---
+++ Understanding-TS/lecture31.ts
@@ -0,0 +1,7 @@
+// Nullish coalescing
+const userInput = null;
+
+// give default fallback value if userInput is null or undefined (nullish value)
+
+const storedData = userInput || "DEFAULT";
+console.log(storedData) // DEFAULT
