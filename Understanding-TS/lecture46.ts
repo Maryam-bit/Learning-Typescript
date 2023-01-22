3f229b73b1fc5a4561dcc238794d3958e39317bf
Maryam-bit <rajputmaryam5555@gmail.com>
2023-01-22 18:13:51 +0500

    import namespace from another file

---
+++ Understanding-TS/lecture46.ts
@@ -0,0 +1,10 @@
+// lets import namespace from lecture45 file here
+
+/// <reference path="lecture45.ts" />
+namespace App {
+    let u2: UserObj = {
+        name: "max",
+        email: "max@gmail.com"
+    }
+    console.log(u2)
+}
